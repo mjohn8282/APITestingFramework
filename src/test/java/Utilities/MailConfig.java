e831a724541ad138211fc3f2b6ea0d5a37af4926
John Moses s <Jmoses@chaingang.local>
2020-07-28 20:27:38 +0530

    changed emailid

--- src/test/java/Utilities/MailConfig.java
+++ src/test/java/Utilities/MailConfig.java
@@ -6,7 +6,7 @@ public class MailConfig {
 
     public static  String from ="smoses1994@gmail.com";
     public static  String password="hjekruxnexfkgmwz";
-    public  static  String[] to ={"smoses1994@gmail.com","jmoses171994@gmail.com"};
+    public  static  String[] to ={"smoses1994@gmail.com","jmoses199417@gmail.com"};
     public static String server="smtp.gmail.com";
     public static String subject="Apitesting Framework Report";
     public static String messagebody="smtp.gmail.com";
